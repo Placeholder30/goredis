45bb504664ca5a18fad96dcd143825426f96a49e
malik <malikoabdulaziz@gmail.com>
2026-04-04 11:10:57 +0100

    refactor impl

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"time"
+
 	"github.com/placeholder30/gredis/store"
 )
 
 func main() {
 	goRedis := store.NewStorage()
-	err := goRedis.Init()
+	err := goRedis.ReplayLog()
 	if err != nil {
 		panic(err)
 	}
+	var futureTime time.Duration = time.Duration(time.Now().Add(time.Hour).UnixNano())
+	_, err = goRedis.SET([]byte("age"), []byte("22"), futureTime)
 
 	err = (goRedis.Deint())
 	if err != nil {
 		panic(err)
 	}
 
+}
+
+type Store interface {
+	GET(item string) any
+	SET(key, value any) string
+	DEL(key ...string) int
 }
--- store/store.go
+++ store/store.go
@@ -3,44 +3,47 @@ package store
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"io"
 	"os"
 	"sync"
+	"time"
 )
 
 type WriteAheadLog os.File
+type Op byte
+type Type byte
 
 type Storage struct {
-	mu            sync.Mutex
+	mu            sync.RWMutex
 	mem           map[string][]byte
+	expiryMap     map[string]time.Duration
 	writeAheadLog *os.File
 }
-type Op byte
 
 const (
 	GET Op = iota + 1
 	DEL
 	SET
+	EXPIRE
+)
+const (
+	STRING Type = iota + 1
+	BOOLEAN
+	INTEGER
+	// LIST
+	// HASH
+	// SETS
 )
 
-func (o Op) String() string {
-	switch o {
-	case GET:
-		return "GET"
-	case DEL:
-		return "DEL"
-	case SET:
-		return "SET"
-	default:
-		return "UNKNOWN"
-	}
-}
-
+// [Op][Type][keylen][valueLen][key][value]
 type Entry struct {
-	Op    Op
-	typ   byte
-	key   []byte
-	value []byte
+	Op         Op
+	typ        Type
+	key        []byte
+	value      []byte
+	expiryTime time.Duration
 }
 
 func DecodeDel(r io.Reader) (*Entry, error) {
@@ -52,11 +55,11 @@ func DecodeDel(r io.Reader) (*Entry, error) {
 		return nil, err
 	}
 	keylen := binary.LittleEndian.Uint32(header[0:4])
-	buffer := make([]byte, keylen)
-	_, err = io.ReadFull(r, buffer)
+	key := make([]byte, keylen)
+	_, err = io.ReadFull(r, key)
 	return &Entry{
 		Op:  DEL,
-		key: buffer,
+		key: key,
 	}, nil
 }
 
@@ -77,6 +80,9 @@ func Decode(r io.Reader) (*Entry, error, bool) {
 	case DEL:
 		entry, _ = DecodeDel(r)
 		return entry, nil, true
+	case EXPIRE:
+		entry, err = DecodeExpiry(r)
+		return entry, nil, true
 	default:
 		return nil, nil, false
 	}
@@ -87,10 +93,11 @@ func Decode(r io.Reader) (*Entry, error, bool) {
 
 func NewStorage() *Storage {
 	return &Storage{
-		mem: make(map[string][]byte),
+		mem:       make(map[string][]byte),
+		expiryMap: make(map[string]time.Duration),
 	}
 }
-func (s *Storage) Init() error {
+func (s *Storage) ReplayLog() error {
 	f, err := os.OpenFile("wal.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		return err
@@ -110,6 +117,7 @@ func (s *Storage) Init() error {
 
 	reader := bufio.NewReader(s.writeAheadLog)
 	for {
+		fmt.Println(mem)
 		entry, err, del := Decode(reader)
 		if err == io.EOF {
 			break
@@ -121,7 +129,6 @@ func (s *Storage) Init() error {
 			mem[string(entry.key)] = entry.value
 		} else {
 			delete(mem, string(entry.key))
-
 		}
 	}
 
@@ -132,6 +139,8 @@ func (s *Storage) Deint() error {
 }
 
 func (s *Storage) GET(item string) []byte {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	val, ok := s.mem[item]
 	if ok {
 		return val
@@ -139,14 +148,30 @@ func (s *Storage) GET(item string) []byte {
 	return nil
 }
 
-func (s *Storage) SET(key, value []byte) (string, error) {
+func (s *Storage) SET(key, value []byte, expiry time.Duration) (string, error) {
+	var hasExpiry bool
+	expiryTime := expiry.Nanoseconds()
+	if expiryTime != 0 {
+		currentTime := time.Now().UnixNano()
+		if currentTime > expiry.Nanoseconds() {
+			return "", errors.New("Invalid time")
+		}
+		s.expiryMap[string(key)] = expiry
+		hasExpiry = true
+	}
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	entry := Entry{Op: SET, key: []byte(key), value: []byte(value)}
-	val := entry.Encode()
+	val := entry.EncodeSet()
 	_, err := s.writeToWAL(val)
 	if err != nil {
 		return "", err
+	}
+
+	if hasExpiry {
+		entry := Entry{Op: EXPIRE, key: key, expiryTime: expiry}
+		expiryText := entry.EncodeExpiry()
+		_, err = s.writeToWAL([]byte(expiryText))
 	}
 	s.mem[string(key)] = value
 
@@ -154,7 +179,6 @@ func (s *Storage) SET(key, value []byte) (string, error) {
 }
 
 func (s *Storage) DEL(keys ...string) (int, error) {
-
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	deleteCount := 0
@@ -176,22 +200,44 @@ func (s *Storage) DEL(keys ...string) (int, error) {
 	return deleteCount, nil
 }
 
-type Store interface {
-	GET(item string) any
-	SET(key, value any) string
-	DEL(key ...string) int
+func (s *Storage) EXPIRE(key string, expiryTime time.Duration) (int, error) {
+	if expiryTime == 0 {
+		return 0, errors.New("invalid expiry")
+	}
+	entry := Entry{Op: EXPIRE, key: []byte(key), expiryTime: expiryTime}
+	expiryText := entry.EncodeExpiry()
+	_, err := s.writeToWAL([]byte(expiryText))
+	if err != nil {
+		return 0, errors.New("failed to write to wal")
+	}
+	return 1, nil
 }
 
-func (s *Storage) writeToWAL(bytes []byte) (int, error) {
-
-	written, err := s.writeAheadLog.Write(bytes)
+// Op-keylen-valuelen-k-v
+func DecodeExpiry(r io.Reader) (*Entry, error) {
+	header := make([]byte, 8)
+	_, err := io.ReadFull(r, header)
 	if err != nil {
-		return 0, err
+
+		return nil, err
+	}
+	keylen := binary.LittleEndian.Uint32(header[0:4])
+	valueLen := binary.LittleEndian.Uint32(header[4:8])
+
+	key := make([]byte, keylen)
+	_, err = io.ReadFull(r, key)
+	if err != nil {
+		return nil, err
 	}
 
-	return written, nil
+	value := make([]byte, valueLen)
+	_, err = io.ReadFull(r, value)
+	if err != nil {
+		return nil, err
+	}
+	valueSec := binary.LittleEndian.Uint32(value)
+	return &Entry{Op: SET, key: key, expiryTime: time.Duration(valueSec)}, nil
 }
-
 func DecodeSet(r io.Reader) (*Entry, error) {
 	header := make([]byte, 8)
 	_, err := io.ReadFull(r, header)
@@ -213,12 +259,10 @@ func DecodeSet(r io.Reader) (*Entry, error) {
 	if err != nil {
 		return nil, err
 	}
-	res := &Entry{Op: SET, key: key, value: value}
-
-	return res, nil
+	return &Entry{Op: SET, key: key, value: value}, nil
 }
 
-func (e *Entry) Encode() []byte {
+func (e *Entry) EncodeSet() []byte {
 	keyLen := uint32(len(e.key))
 	valueLen := uint32(len(e.value))
 
@@ -247,4 +291,33 @@ func (e *Entry) EncodeDel() []byte {
 	buffer = append(buffer, temp...)
 	buffer = append(buffer, e.key...)
 	return buffer
+}
+
+// TODO
+// for expiry, value will be encoded using the expiry field.
+func (e *Entry) EncodeExpiry() []byte {
+	keylen := uint32(len(e.key))
+	valueLen := uint32(len(e.expiryTime.String())) //FIXME
+	buffer := make([]byte, 0)
+	buffer = append(buffer, byte(e.Op))
+
+	temp := make([]byte, 4)
+	binary.LittleEndian.PutUint32(temp, keylen)
+	buffer = append(buffer, temp...)
+	binary.LittleEndian.AppendUint32(temp, valueLen)
+	buffer = append(buffer, temp...)
+
+	buffer = append(buffer, e.key...)
+	buffer = append(buffer, byte(e.expiryTime))
+	return buffer
+}
+
+func (s *Storage) writeToWAL(bytes []byte) (int, error) {
+
+	written, err := s.writeAheadLog.Write(bytes)
+	if err != nil {
+		return 0, err
+	}
+
+	return written, nil
 }
